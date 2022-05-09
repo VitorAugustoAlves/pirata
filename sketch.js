e41a612bc4a61328ff2d1c73087eba60c4d4641b
VitorAugustoAlves <100448405+VitorAugustoAlves@users.noreply.github.com>
2022-05-09 20:01:24 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -46,7 +46,8 @@ function draw() {
   image(towerImage, tower.position.x, tower.position.y, 160, 310);
   pop();
   for (var i = 0; i <bolas.length; i++){
-    bolass(bolas[i])
+    bolass(bolas[i],i)
+    colision(i)
   }
   cannon.display();
   sbarcos()
@@ -64,9 +65,13 @@ function keyPressed() {
     bolas.push(bola);
   }
 }
-function bolass (bola){
+function bolass (bola,index){
   if (bola){
     bola.display();
+    console.log("função de bolas")
+    if  (bola.body.position.x>=width||bola.body.position.y>=height-50){
+      bola.remove(index)
+    }
   }
 }
 function sbarcos (){
@@ -80,11 +85,24 @@ function sbarcos (){
     for(var i=0;i<barcoss.length;i++){
       if(barcoss[i]){
         Matter.Body.setVelocity(barcoss[i].body, {x:-0.7,y:0})
-        barcoss.display();    
+        barcoss[i].display();    
       }
     }
   }else{
     barco = new Barcos(width-79, height-70, 170, 170,-60)
     barcoss.push(barco)
+  }
+}
+function colision (index){
+  for(var i=0;i<barcoss.length;i++){
+    if  (bolas[index]!==undefined&&barcoss[i]!==undefined){
+      var bolabarco = Matter.SAT.collides(bolas[index],barcoss[i])
+        if  (bolabarco.collided){
+          barcoss[i].remove(i)
+            Matter.World.remove(world, bolas[index].body)
+              delete bolas[index]
+
+        }
+    }
   }
 }
